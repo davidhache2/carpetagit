421070d740cd15c9dedd767efbfffa937c4640e5
davidhache2 <david.disenioweb@gmail.com>
2015-02-06 14:00:08 +0100

    No se

--- gasolinaverde/index.php
+++ gasolinaverde/index.php
@@ -1,93 +1,93 @@
-<!DOCTYPE html>
-<html  lang="es">
-	<head>
-		<meta charset='utf-8'>
-		<title>Gasolina verde</title>
-		<meta charset="utf-8" >
-		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-		<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.1/darkly/bootstrap.min.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
-	</head>
-	<body>
-		<header class="container">
-			<img class="logo" src="images/logo.jpg" alt="logo"/>
-			<div class="titulo">
-					<h1>Gasolina Verde</h1>
-					
-					<form method="POST" action="index.php" class="login">
-						<label class="salto" for="usuario">Iniciar sesión</label>
-						<input type="text" name="usuario" placeholder="Usuario"/>
-						<input type="password" name="password" placeholder="Contraseña"/>
-						<input type="button" value="Enviar"/>
-						<div class="recordar"><input type="checkbox" name="recordar" value="recordar"/> Recordar contraseña en este equipo.</div>
-					</form>
-					<p class="dinamico">Frase aleatoria de Gasolina Verde</p>
-				</div>
-					
-				<nav>
-					<ul>
-						<li><a href="#">Inicio</a></li>
-						<li><a href="#">Historia</a></li>
-						<li><a href="#">Discografía</a></li>
-						<li><a href="#">Tour</a></li>
-						<li><a href="#">Contacto</a></li>
-					</ul>
-				</nav>		
-		</header>
-		<!--FIN HEADER-->
-		<!--INICIO MAIN-->
-		<section class="container"  id="bloque">
-				<section class="sectionvideo">
-					<h2 class="video">Video promocional</h2>
-					<video src="media/domingoresaka.mp4" controls>Error de reproducción</video>
-				</section>
-				<section id="multimedia">
-			<h2 class="multimedia">Area multimedia</h2>
-			
-			<p>
-				<ul class="galeria">
-					<li><a href="images/tour1.jpg"><img src="images/thumbnails/tour1.jpg" alt="tour"/></a></li>
-					<li><a href="images/tour2.jpg"><img src="images/thumbnails/tour2.jpg" alt="tour"/></a></li>
-					<li><a href="images/tour3.jpg"><img src="images/thumbnails/tour3.jpg" alt="tour"/></a></li>
-					<li><a href="images/tour4.jpg"><img src="images/thumbnails/tour4.jpg" alt="tour"/></a></li>
-				</ul>
-			</p>
-				
-			<p>
-				<ul class="galeria">
-					<li><a href="images/tour5.jpg"><img src="images/thumbnails/tour5.jpg" alt="tour"/></a></li>
-					<li><a href="images/tour6.jpg"><img src="images/thumbnails/tour6.jpg" alt="tour"/></a></li>
-					<li><a href="images/tour7.jpg"><img src="images/thumbnails/tour7.jpg" alt="tour"/></a></li>
-					<li><a href="images/tour8.jpg"><img src="images/thumbnails/tour8.jpg" alt="tour"/></a></li>
-				</ul>
-			</p>
-			</section>
-		</section>
-		<!-- FIN MAIN-->
-		<!-- INICIO SIDEBAR -->
-		<section class="container">
-			<h2>Libro de visitas</h2>
-			<form action="index.php" method="POST">
-				<label for="nombre">Nombre o apodo: </label> <br/>
-				<input type="text" name="nombre"/> <br/>
-				<label for="comentario">Comentario: </label><br/>
-				<textarea name="comentario"></textarea> <br/>
-				<input class="enviar" type="submit" value="Publicar">
-			</form>
-		</section>
-		<!--FIN SIDEBAR-->
-		<section class="container">
-			<h2>Comentarios</h2>
-				<div class="datos">
-				<p id="nombre">Pablito</p>
-				<hr/>
-				<p id="comentario">Hola Gasolina Verde soys unos gayses vuestra página apesta y vosotros no os duchais guarros.</p>
-				</div>
-		</section>
-		<!--INICIO FOOTER-->
-		<footer class="container">
-			<p>Página web diseñada por <strong>Juan Carlos Martinez Vazquez</strong> y <strong>David Martín Rodríguez</strong> para el grupo <strong>&copy; Gasolina Verde</strong> || 2015 || @Todos los derechos reservados.</p>
-		</footer>
-		<!--FIN FOOTER-->
-	</body>
+﻿<!DOCTYPE html>
+<html lang='es'>
+<head>
+	<meta charset="UTF-8" />
+	<title>Gasolina Verde</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.1/darkly/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body class="container">
+<!-- Inicio header -->
+<header>
+		<img src="images/logo.jpg" class="logo" alt="logo"/>
+		<div class="titulo">
+		<h1>Gasolina Verde</h1>
+		<form method='POST' action='admin/index.php' class="login">
+		<label class="salto" for="usuario">Iniciar Sesión</label>
+		<input type="text" name="usuario" placeholder="Usuario"/>
+		<input type="password" name="contrasena" placeholder="Contraseña"/>
+		<input type="submit" value="Enviar">
+		<div class="recordar" ><input type="checkbox"  name="recordar" value="recordar"/> Recordar contraseña en este equipo.</div>
+		</form>
+		<p class="dinamico">
+		<?php
+		include 'randomtxt/functions.php';
+		randomtxt();
+		?>
+		</p>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Historia</a></li>
+				<li><a href="#">Discografía</a></li>
+				<li><a href="#">Tour</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+</header>	
+<!-- Fin header-->
+<!-- Inicio main-->
+	<section id="bloque">
+	<section class="sectionvideo">
+	<h2 class="video">Video Promocional</h2>
+	<video src="media/domingoresaka.mp4" controls>Error de Reproducción</video>
+	</section>
+	<h2 class="multimedia">Area Multimedia</h2>
+	<div>
+		<ul class="galeria">
+		<li><a href="images/tour1.jpg"><img src="images/thumbnails/tour1.jpg" alt="tour"/></a></li>
+		<li><a href="images/tour2.jpg"><img src="images/thumbnails/tour2.jpg" alt="tour"/></a></li>
+		<li><a href="images/tour3.jpg"><img src="images/thumbnails/tour3.jpg" alt="tour"/></a></li>
+		<li><a href="images/tour4.jpg"><img src="images/thumbnails/tour4.jpg" alt="tour"/></a></li>
+		<li><a href="images/tour5.jpg"><img src="images/thumbnails/tour5.jpg" alt="tour"/></a></li>
+		<li><a href="images/tour6.jpg"><img src="images/thumbnails/tour6.jpg" alt="tour"/></a></li>
+		<li><a href="images/tour7.jpg"><img src="images/thumbnails/tour7.jpg" alt="tour"/></a></li>
+		<li><a href="images/tour8.jpg"><img src="images/thumbnails/tour8.jpg" alt="tour"/></a></li>
+		</ul>
+	</div>
+	</section>
+<!--Fin Main-->
+<!--Inicio Sidebar-->
+	<section>
+	<h2>Libro de Visitas</h2>
+		<form action="index.php" method="POST">
+		<label for="nombre">Nombre o ápodo</label><br>
+		<input type="text" name="nombre"><br>
+		<label for="comentario">Comentario</label><br>
+		<textarea id="noresize" name="comentario"></textarea><br>
+		<input type="submit" value="Publicar" class="enviar">
+		</form>
+	</section>
+<!--Fin Sidebar -->	
+	<section id="comentarios">
+		<div class="visitas">Nº de visitas: <span><?php visitas();?></span></div>
+		<h2>Comentarios</h2>
+		<div class="datos">
+			<?php procesarcomentarios(); ?>
+		</div>	
+	</section>
+<!--Fin Footer -->	
+	<footer>
+		<ul class="contact">
+			<li><a href="https://twitter.com"/> <img src="images/iconos/twitter.png"/><span class="label">Twitter</span></a></li>
+			<li><a href="https://es.linkedin.com"/> <img src="images/iconos/linkedin.png"/><span class="label">Linkedin</span></a></li>
+			<li><a href="https://es-es.facebook.com/"/> <img src="images/iconos/facebook.png"/><span class="label">Facebook</span></a></li>
+			<li><a href="https://es.pinterest.com/"/> <img src="images/iconos/pinterest.png"/><span class="label">Pinterest</span></a></li>
+			<li><a href="https://www.youtube.com"/> <img src="images/iconos/youtube.png"/><span class="label">Youtube</span></a></li>
+		</ul>
+		<p>Página web diseñada por <strong>David Martín Rodríguez</strong> y <strong> Juan Carlos Martínez Vázquez</strong> para el grupo <strong>&copy; Gasolina Verde</strong> ||2015|| @Todos los derechos reservados. </p>
+	</footer>
+</body>
 </html>
